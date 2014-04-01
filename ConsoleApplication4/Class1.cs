07192f5422f614cd40b17a7604dda42a9db35bfd
flyfly <becausethespiderspiedher@gmail.com>
2014-04-01 12:31:06 +0000

    2nd commit

--- ConsoleApplication4/Class1.cs
+++ ConsoleApplication4/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication4
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            // hoge hoge
-        }
     }
 }
