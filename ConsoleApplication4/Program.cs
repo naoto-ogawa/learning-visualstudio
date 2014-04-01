cb74cdecf36e387969f941bd6eac24f9e60e17e4
flyfly <becausethespiderspiedher@gmail.com>
2014-04-01 13:13:59 +0000

    add debug messagee

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace ConsoleApplication4
         static void Main(string[] args)
         {
             // hoge hoge
+
+            Debug.WriteLine("Hello World");
         }
     }
 }
